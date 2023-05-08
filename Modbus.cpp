b11185755406647e2ebc2f73b9307e6e74f34521
Lars Lindner <lindnerlars81@gmail.com>
2023-05-08 20:13:49 +0200

    First Commit on Main

---
+++ Modbus.cpp
@@ -0,0 +1,17 @@
+// Modbus.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
+//
+
+#include <iostream>
+#include "libmodbus\modbus.h"
+
+//extern "C" {
+//    #include <modbus.h>
+//}
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
